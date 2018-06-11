2f9b04c573e350c8681b772909e0265b159034ee
Zlatko Radojcic <zlatko.rad@gmail.com>
2018-06-11 22:26:40 +0200

    Just Hello World part of the program. First version that can be built.

---
+++ DoTheRightString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DoTheRightString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            CloseOnKeyPress();
+        }
+
+        static void CloseOnKeyPress()
+        {
+            Console.WriteLine("Press any key to close screen");
+            Console.ReadKey();
+            System.Environment.Exit(1);
+        }
+    }
+}
